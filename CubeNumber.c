075bc5de0ed964975e0ddf4a9000c45b4090de0e
Arash Sa <101516570+Arashsa31@users.noreply.github.com>
2022-04-16 23:41:35 -0400

    Add files via upload

---
+++ CubeNumber.c
@@ -0,0 +1,37 @@
+/**
+ * @author Arash
+ * Write a program that displays the first n cubes. (The number n is entered by the user.)
+ * In the example below, the user's input is underlined.
+ * How many cubes? 7
+
+ * 1 cubed is 1
+ * 2 cubed is 8
+ * 3 cubed is 27
+ * 4 cubed is 64
+ * 5 cubed is 125
+ * 6 cubed is 216
+ * 7 cubed is 343
+ */
+
+#include <stdio.h>
+int main()
+{
+	//variables
+	int n, tempCount, counter = 1;
+
+	//input
+	printf("How many cubes? ");
+	scanf_s("%i", &n);
+
+		//calculation
+	while (counter <= n)
+	{
+		tempCount = counter * counter * counter;
+		printf("%i cubed is %i\n", counter, tempCount);
+		counter++;
+	}
+
+	//output
+
+	return 0;
+}
